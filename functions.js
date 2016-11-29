31d3e727672d1d0aa753da08de52af99ef22f690
Alison Wu <awu@cfri.ca>
2016-11-29 14:02:44 -0800

    initial commit

---
+++ functions.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){  
+
+  // RSS feed
+  var feed = "http://bulletins.it.ubc.ca/archives/category/security/feed";
+
+  // Use Yahoo Query Language (YQL) https://developer.yahoo.com/yql/
+  // Take the provided feed, and add it to a YQL query.
+  var yql = 'http://query.yahooapis.com/v1/public/yql?q='
+    + encodeURIComponent('select * from rss where url="' + feed + '"')
+    + '&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+  console.log(yql);
+
+  $.ajax({
+    url: yql,
+    dataType: "XML",
+    success: function(data) {
+      console.log(data);
+
+      var limit = 10, // set feed limit here
+      temp = limit+1;
+      count = 0;
+      $(data).find("item").each(function() {
+      if (limit==0) return;
+      count = temp-limit;
+      var item = $(this);
+      var title = item.find('title').text();
+      var link = item.find('link').text();
+      var date = item.find('pubDate').text().substr(0, 11);
+
+      console.log("--- article #" + count + "---");
+      console.log("title      : " + title);
+      console.log("link       : " + link);
+      console.log("pubDate    : " + date);
+      console.log("description: " + item.find("description").text());
+
+      var rssRow = "<tr>"+
+      "<td id='rssItem'><p><span id='rssDate'>"+date+" | </span>"+
+      "<a href='"+link+"' id='rssTitle'>"+title+"</a></p></td>"+
+      "</tr>";
+      $("#rssFeed").append(rssRow);
+      limit--;
+      });
+    },
+    error: function(error) {
+      $('#rssFeed').append("<p id='rssError'>RSS Feed cannot load</p>");
+    }
+  });
+
+});
